72271f873a00eb0a2a83c3c1934a2ad73c6ca7ab
Frrank Sarabia <frrankemergk@hotmail.com>
2024-08-15 15:46:06 -0500

    modificacion en footer

--- detalles.js
+++ detalles.js
@@ -9,15 +9,15 @@ let uuid = new URLSearchParams (window.location.search).get ("id")
 
   document.getElementById ("Detalles").innerHTML = ` 
 
- <div class="card Personaje d-flex flex-column flex-lg-row justify-content-between align-content-center">
+ <div class="card Personaje d-flex flex-column flex-lg-row justify-content-center align-content-center">
      
      <div class=" textoDetalles mx-5 py-2 d-flex flex-column justify-content-center">
       <h2 class="text-center text-light">${detalles.displayName}</h2>
       <p class="text-center text-light">${detalles.description}</p>
     </div>
-    <div class="my-2 ms-5">
-          <img src="${detalles.fullPortrait}" class="card-img" >
-     </div>
+    <div class="d-flex justify-content-start">
+     <img src="${detalles.fullPortrait}" class="card-img"  >
+    </div>
  </div>  
  `
 
